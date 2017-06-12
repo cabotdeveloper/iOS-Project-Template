f0989ee9ccc9f0aa402589dfd30277920cbf9278
Shibin Moideen <shibin.moideen@cabotsolutions.com>
2017-06-12 11:31:52 +0530

    Initial Commit

---
+++ iOSTemplate/Modules/Registration/Models/RegisterResponse.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterResponse.swift
+//  iOSTemplate
+//
+//  Created by Shibin Moideen on 6/12/17.
+//  Copyright © 2017 Shibin Moideen. All rights reserved.
+//
+
+import UIKit
+
+class RegisterResponse: NSObject {
+
+}
---
+++ iOSTemplate/WebService/APIController.swift
@@ -0,0 +1,16 @@
+//
+//  APIController.swift
+//  iOSTemplate
+//
+//  Created by Shibin Moideen on 6/12/17.
+//  Copyright © 2017 Shibin Moideen. All rights reserved.
+//
+
+import UIKit
+
+class APIController: NSObject {
+
+    // This class will list all the API functions need to be implemented in the application.
+    // All API calls should go through this class
+    // Common features like parsing the response will be implemented here.
+}
