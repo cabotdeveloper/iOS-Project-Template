985c1a72c6c0a1368c5fad3af224cc4b9084455d
Denny Mathew <denny.mathew@cabotsolutions.com>
2017-08-08 14:58:10 +0530

    + Base64 Encode/Decode + Email Validation

--- iOSTemplate/Modules/Crypto/String+AES.swift
+++ iOSTemplate/Modules/Crypto/String+AES.swift
@@ -12,21 +12,21 @@ import CryptoSwift
 extension String {
     
     //MARK:- Encrypt/Decrypt
-//    func aesEncrypt(key: String, iv: String) throws -> String {
-//        let data = self.data(using: .utf8)!
-//        let encrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).encrypt([UInt8](data))
-//        let encryptedData = Data(encrypted)
-//
-//        return encryptedData.base64EncodedString()
-//    }
-//    
-//    func aesDecrypt(key: String, iv: String) throws -> String {
-//        let data = Data(base64Encoded: self)!
-//        let decrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).decrypt([UInt8](data))
-//        let decryptedData = Data(decrypted)
-//
-//        return String(bytes: decryptedData.bytes, encoding: .utf8) ?? "Could not decrypt"
-//    }
+    func aesEncrypt(key: String, iv: String) throws -> String {
+        let data = self.data(using: .utf8)!
+        let encrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).encrypt([UInt8](data))
+        let encryptedData = Data(encrypted)
+
+        return encryptedData.base64EncodedString()
+    }
+    
+    func aesDecrypt(key: String, iv: String) throws -> String {
+        let data = Data(base64Encoded: self)!
+        let decrypted = try! AES(key: key, iv: iv, blockMode: .CBC, padding: PKCS7()).decrypt([UInt8](data))
+        let decryptedData = Data(decrypted)
+
+        return String(bytes: decryptedData.bytes, encoding: .utf8) ?? "Could not decrypt"
+    }
     
     //MARK:- Encode/Decode - Base64
     func base64Encoded() -> String {
